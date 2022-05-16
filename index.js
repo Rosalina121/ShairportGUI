49485c14022f3952361949f1c9a89c8de3661ae9
Alina Rosa <rosalina@palitechnika.com>
2022-05-16 16:48:16 +0200

    framework done

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log("hello world");
+// import the module
+let ShairportReader = require("shairport-sync-reader");
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+// read from pipe
+const pipeReader = new ShairportReader({
+    path: "../../cygwin64/tmp/shairport-sync-metadata.lnk",
+});
+
+// listen for metadata
+pipeReader.on("meta", function (metadata) {
+    console.log('dupa');
+    console.log(metadata);
+    // parse metadata
+    io.emit("metadata", metadata);
+});
+
+pipeReader.on("PICT", function(pictureData) {
+    console.log(pictureData);
+    // to base64
+    io.emit("pictureData", pictureData);
+})
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
+
+server.listen(3000, () => {
+    console.log("listening on *:3000");
+});
