cbdc1cf7499dd21e2daeba24513ca1dc3660d6e7
Alina Rosa <rosalina@palitechnika.com>
2022-05-27 18:37:47 +0200

    uncomment the core fucking functionality of the app

--- index.js
+++ index.js
@@ -41,26 +41,26 @@ const vibrantTest = (image) => {
 
 vibrantTest("https://test.palitechnika.com/rosa.png");
 
-// // read from pipe
-// var pipeReader = new ShairportReader({ path: '/tmp/shairport-sync-metadata' });
+// read from pipe
+var pipeReader = new ShairportReader({ path: '/tmp/shairport-sync-metadata' });
 
-// // listen for metadata
-// pipeReader.on("meta", function (metadata) {
-//     console.log('got meta: ');
-//     // parse metadata
-//     let title = metadata.minm;
-//     let artist = metadata.asar;
-//     let album = metadata.asal;
-//     console.log(title + " - " + artist + " - " + album);
-//     io.emit("metadata", {title, artist, album});
-// });
+// listen for metadata
+pipeReader.on("meta", function (metadata) {
+    console.log('got meta: ');
+    // parse metadata
+    let title = metadata.minm;
+    let artist = metadata.asar;
+    let album = metadata.asal;
+    console.log(title + " - " + artist + " - " + album);
+    io.emit("metadata", {title, artist, album});
+});
 
-// pipeReader.on("PICT", function(pictureData) {
-//     console.log(pictureData);
-//     // pirctureData to base64
-//     let base64 = Buffer.from(pictureData).toString('base64');
-//     io.emit("pictureData", base64);
-// })
+pipeReader.on("PICT", function(pictureData) {
+    console.log(pictureData);
+    // pirctureData to base64
+    let base64 = Buffer.from(pictureData).toString('base64');
+    io.emit("pictureData", base64);
+})
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
