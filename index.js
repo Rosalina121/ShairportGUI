3ff31209c83160b03a54d8b9c79d765f4ead5d78
Alina Rosa <rosalina@palitechnika.com>
2022-05-27 21:59:42 +0200

    preetier

--- index.js
+++ index.js
@@ -5,19 +5,17 @@ const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
 const vibrant = require("node-vibrant");
-const fs = require("fs")
-
+const fs = require("fs");
 
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "192.168.1.181:3000",   // change this to your local ip
+        origin: "192.168.1.181:3000", // change this to your local ip
         methods: ["GET,POST"],
         credentials: true,
-        transports: ['websocket', 'polling'],
+        transports: ["websocket", "polling"]
     },
     allowEIO3: true
-
 });
 
 const saveBase64ImageToDisk = (data, fileName) => {
@@ -39,7 +37,7 @@ const emitPalette = (image) => {
             tlShadowColor: palette.DarkVibrant.rgb,
             borderColor: palette.LightVibrant.rgb,
             brShadowColor: palette.DarkMuted.rgb,
-            artistColor: palette.LightMuted.rgb,
+            artistColor: palette.LightMuted.rgb
         };
         console.log(paletteObj);
         io.emit("palette", paletteObj);
@@ -47,49 +45,49 @@ const emitPalette = (image) => {
 };
 
 const removeBrackets = (str) => {
-    str = str.replace(/\[[^]*\]/,'');
-    return str.replace(/\([^]*\)/,'');
+    str = str.replace(/\[[^]*\]/, "");
+    return str.replace(/\([^]*\)/, "");
 };
 
 // read from pipe
-var pipeReader = new ShairportReader({ path: '/tmp/shairport-sync-metadata' }); // your metadata pipe path
+var pipeReader = new ShairportReader({ path: "/tmp/shairport-sync-metadata" }); // your metadata pipe path
 
 // listen for metadata
 pipeReader.on("meta", function (metadata) {
-    console.log('got meta: ');
+    console.log("got meta: ");
     // parse metadata
     let title = metadata.minm;
     let artist = metadata.asar;
     let album = metadata.asal;
-    title = removeBrackets(title);  // get rid of brackets coz text is too long
-    album = removeBrackets(album);  // same
+    title = removeBrackets(title); // get rid of brackets coz text is too long
+    album = removeBrackets(album); // same
     console.log(title + " - " + artist + " - " + album);
-    io.emit("metadata", {title, artist, album});
+    io.emit("metadata", { title, artist, album });
 });
 
-pipeReader.on("PICT", function(pictureData) {
+pipeReader.on("PICT", function (pictureData) {
     console.log(pictureData);
     // pirctureData to base64
-    let base64 = Buffer.from(pictureData).toString('base64');
+    let base64 = Buffer.from(pictureData).toString("base64");
     // save to disk
-    saveBase64ImageToDisk(base64, './image.png').then((fileName) => {
+    saveBase64ImageToDisk(base64, "./image.png").then((fileName) => {
         console.log(fileName);
         // get palette
         emitPalette(fileName);
     });
     io.emit("pictureData", base64);
-})
+});
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
+io.on("connection", (socket) => {
+    console.log("a user connected");
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
     });
-  });
+});
 
 server.listen(3000, () => {
     console.log("listening on *:3000");
