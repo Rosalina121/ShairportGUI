4ab3d4a4f08b938f41d4f500a0860af71d7ad387
JaroslawPokropinski <jarek.pokropinski@gmail.com>
2022-06-16 00:10:11 +0200

    fix sending shairport meta

--- src/ShairportProvider.js
+++ src/ShairportProvider.js
@@ -13,32 +13,13 @@ class ShairportProvider extends SongProvider {
         this.pipeReader.on("meta", function (metadata) {
             console.log("got meta: ");
             // parse metadata
-            let title = metadata.minm;
-            let artist = metadata.asar;
-            let album = metadata.asal;
-            title = removeBrackets(title); // get rid of brackets coz text is too long
-            album = removeBrackets(album); // same
-            console.log(title + " - " + artist + " - " + album);
-            io.emit("metadata", { title, artist, album });
+            emitSongMeta(metadata.minm, metadata.asar, metadata.asal);
         });
 
         this.pipeReader.on("PICT", function (pictureData) {
             console.log(pictureData);
             // pirctureData to base64
-            let base64 = Buffer.from(pictureData).toString("base64");
-            // save to disk and emit palette
-            try {
-                saveBase64ImageToDisk(base64, "./image.png").then(
-                    (fileName) => {
-                        console.log(fileName);
-                        // get palette
-                        emitPalette(fileName);
-                    }
-                );
-                io.emit("pictureData", base64);
-            } catch (err) {
-                console.log(err);
-            }
+            emitSongCover(pictureData);
         });
     }
 }
